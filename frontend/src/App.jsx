e88772679bc5d0f8e7fc028935bd8e0816262296
NguyenTruongGiang <23130082@st.hcmuaf.edu.vn>
2026-09-23 14:24:24 +0700

    style: giao diện Đăng kí

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import CustomerRegisterPage from './pages/CustomerRegisterPage'
+import PartnerRegisterPage from './pages/PartnerRegisterPage'
 
 function App() {
-  return (<><LoginPage/></>)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<CustomerRegisterPage />} />
+        <Route path="/register-partner" element={<PartnerRegisterPage />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 export default App
